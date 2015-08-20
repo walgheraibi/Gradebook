3d5cc3cad15c9caf0c5cc03171cbb63dd8765535
Weaam <w.algheraibi@hotmail.com>
2015-08-20 16:16:43 -0400

    Gradebook

---
+++ Student/WebContent/input.jsp
@@ -0,0 +1,187 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Grade Form</title>
+
+<link rel="stylesheet"
+	href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<script
+	src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script
+	src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+
+	<ul class="nav nav-tabs">
+		<li role="presentation" class="active"><a data-toggle="tab"
+			href="#form">Add Assignment </a></li>
+		<li role="presentation"><a data-toggle="tab" href="#byStudent">
+				assignments by a student</a></li>
+		<li role="presentation"><a data-toggle="tab" href="#byType">
+				assignments by a type </a></li>
+					<li role="presentation"><a data-toggle="tab" href="#bySandT">
+				assignments by a type & Student</a></li>
+	</ul>
+	<div class="tab-content">
+
+		<div id="form" class="tab-pane fade in active">
+			<div class="container">
+				<br /> <br />
+
+				<div class="jumbotron">
+					<center>
+						<h1>Add Assignment Form</h1>
+					</center>
+					<form class="form-horizontal" action="Studentform" method="post">
+						<input type="hidden" name="action" value="add">
+						<div class="form-group">
+							<input type="text" class="form-control" id="studentid"
+								name="studentid" placeholder="Student ID" required>
+						</div>
+						<div class="form-group">
+							<input type="text" class="form-control" id="assignmentname"
+								name="assignmentname" placeholder="Assignment Name" required>
+						</div>
+						<div class="form-group">
+							<div class="dropdown">
+								<select class=" btn btn-primary btn-lg " name="type"
+									text="Assignment Type">
+									<option class=" btn btn-primary btn-lg " name="type"
+										value="homework">homework</option>
+									<option class=" btn btn-primary btn-lg " name="type"
+										value="quiz">quiz</option>
+									<option class=" btn btn-primary btn-lg " name="type"
+										value="test">test</option>
+									<option class=" btn btn-primary btn-lg " name="type"
+										value="project">project</option>
+								</select>
+							</div>
+						</div>
+
+						<div class="form-group">
+							<input type="text" class="form-control" id="date" name="date"
+								placeholder="Date (MM-DD-YYYY)" required>
+						</div>
+						<div class="form-group">
+							<input type="text" class="form-control" id="grade" name="grade"
+								placeholder="Grade" required>
+						</div>
+						<div class="form-group">
+							<input type="submit" class="btn btn-primary btn-lg btn-block"
+								value="Add Student">
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+
+		<div id="byStudent" class="tab-pane fade">
+			<div class="container">
+				<br /> <br />
+
+				<div class="jumbotron">
+					<center>
+						<h1>assignments by a student</h1>
+					</center>
+					<form class="form-horizontal" action="Studentform" method="post">
+						<input type="hidden" name="action" value="byStudent">
+						<div class="form-group">
+							<input type="text" class="form-control" id="getstudentid"
+								name="getstudentid" placeholder="Student ID" required>
+						</div>
+						<div class="form-group">
+							<input type="submit" class="btn btn-primary btn-lg btn-block"
+								value="Get All Assignment for this Student">
+						</div>
+					</form>
+					${assignmentbyid}
+				</div>
+			</div>
+		</div>
+
+		<div id="byType" class="tab-pane fade">
+			<div class="container">
+				<br /> <br />
+
+				<div class="jumbotron">
+					<center>
+						<h1>assignments by a type</h1>
+					</center>
+					<form class="form-horizontal" action="Studentform" method="post">
+						<input type="hidden" name="action" value="byType">
+						<div class="form-group">
+							<div class="dropdown">
+								<select class=" btn btn-primary btn-lg " name="assigntype"
+									text="Assignment Type">
+									<option class=" btn btn-primary btn-lg " name="assigntype"
+										value="homework">homework</option>
+									<option class=" btn btn-primary btn-lg " name="assigntype"
+										value="quiz">quiz</option>
+									<option class=" btn btn-primary btn-lg " name="assigntype"
+										value="test">test</option>
+									<option class=" btn btn-primary btn-lg " name="assigntype"
+										value="project">project</option>
+								</select>
+							</div>
+						</div>
+
+						<div class="form-group">
+							<input type="submit" class="btn btn-primary btn-lg btn-block"
+								value="Get All Assignment for this Type">
+						</div>
+					</form>
+					${assignmentbytype}
+				</div>
+			</div>
+		</div>
+
+		<div id="bySandT" class="tab-pane fade">
+			<div class="container">
+				<br /> <br />
+
+				<div class="jumbotron">
+					<center>
+						<h1>assignments by a type & Student</h1>
+					</center>
+					<form class="form-horizontal" action="Studentform" method="post">
+						<input type="hidden" name="action" value="bySandT">
+
+						<div class="form-group">
+							<input type="text" class="form-control" id="getsid" name="getsid"
+								placeholder="Student ID" required>
+						</div>
+
+						<div class="form-group">
+							<div class="dropdown">
+								<select class=" btn btn-primary btn-lg " name="assignmtype"
+									text="Assignment Type">
+									<option class=" btn btn-primary btn-lg " name="assignmtype"
+										value="homework">homework</option>
+									<option class=" btn btn-primary btn-lg " name="assignmtype"
+										value="quiz">quiz</option>
+									<option class=" btn btn-primary btn-lg " name="assignmtype"
+										value="test">test</option>
+									<option class=" btn btn-primary btn-lg " name="assigntype"
+										value="project">project</option>
+								</select>
+							</div>
+						</div>
+
+						<div class="form-group">
+							<input type="submit" class="btn btn-primary btn-lg btn-block"
+								value="Get All Assignment for this Type and This Student">
+						</div>
+					</form>
+					${assignmentbyst}
+				</div>
+			</div>
+		</div>
+
+	</div>
+</body>
+
+</html>
---
+++ Student/src/Studentform.java
@@ -0,0 +1,195 @@
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * Servlet implementation class Studentform
+ */
+@WebServlet("/Studentform")
+public class Studentform extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Studentform() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		 
+	    //  getServletContext().getRequestDispatcher("/output.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		ResultSet result =null;
+		Connection conn = null;
+		 try {
+				Class.forName("oracle.jdbc.driver.OracleDriver");
+		
+		 String url = "jdbc:oracle:thin:testuser/password@localhost"; 
+	      
+	        //properties for creating connection to Oracle database
+	        Properties props = new Properties();
+	        props.setProperty("user", "testdb");
+	        props.setProperty("password", "password");
+	      
+	        //creating connection to Oracle database using JDBC
+	        
+	    	conn = DriverManager.getConnection(url,props);
+		 }
+	    	 catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			  
+		String sql =""; 
+	
+		
+	        if(request.getParameter("action").equals("add"))
+	        {
+	        	String studentid=request.getParameter("studentid");
+	   		 String grade=request.getParameter("grade");
+	   		 String assignmentname=request.getParameter("assignmentname");
+	   		 String type=request.getParameter("type");
+	   		 String date=request.getParameter("date");
+	   		 sql ="insert into student (studentid,assignmentname,homeworktype, assignmentdate,grade) values("+
+						studentid +",'"+assignmentname+"','"+type+"', TO_DATE('"+date+"','MM-DD-YYYY'),"+Double.parseDouble(grade)+")";
+	   		getResult(sql, conn);
+	        }
+	        else if(request.getParameter("action").equals("byStudent"))
+	        {
+	        	int count =0;
+	        	double avg = 0;
+	        	String assignmentbyid ="<table class=\"table\"><thead><tr><th>ASSIGNMENTNAME</th><th>HOMEWORKTYPE</th><th>ASSIGNMENTDATE</th><th>GRADE</th></tr></thrad><tbody>";
+	        	String studentid=request.getParameter("getstudentid");
+	        	 sql ="select * from student where studentid="+Integer.parseInt(studentid);
+	        	 result = getResult(sql, conn);
+	        	
+	        	 try {
+
+					while(result.next())
+					 {	
+						assignmentbyid+="<tr><td>"+result.getString("ASSIGNMENTNAME")+"</td><td>"+result.getString("HOMEWORKTYPE")+"</td><td>"+result.getDate("ASSIGNMENTDATE")+"</td><td>"+result.getDouble("GRADE")+"</td></tr>"; 
+						avg+= result.getDouble("GRADE");
+						count++;
+					 }
+					avg = (avg/count);
+					assignmentbyid+="</tbody></table><br><h2>Student Average is: " + avg+"</h2>";
+				      response.setContentType("text/html");
+					  request.setAttribute("assignmentbyid",assignmentbyid);
+				   
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+	        	 
+	        }
+			
+	        else if(request.getParameter("action").equals("byType"))
+	        {
+	        	double lowest =0;
+	        	double highest =0;
+	        	String assignmentbytype ="<table class=\"table\"><thead><tr><th>ASSIGNMENTNAME</th><th>HOMEWORKTYPE</th><th>ASSIGNMENTDATE</th><th>GRADE</th></tr></thrad><tbody>";
+	        	String assigntype=request.getParameter("assigntype");
+	        	 sql ="select * from student where HOMEWORKTYPE='"+assigntype+"'";
+	        	 result = getResult(sql, conn);
+	        	
+	        	 try {
+
+					while(result.next())
+					 {	
+						assignmentbytype+="<tr><td>"+result.getString("ASSIGNMENTNAME")+"</td><td>"+result.getString("HOMEWORKTYPE")+"</td><td>"+result.getDate("ASSIGNMENTDATE")+"</td><td>"+result.getDouble("GRADE")+"</td></tr>"; 
+						if(lowest==0)
+							lowest =result.getDouble("GRADE");
+						else if(lowest>result.getDouble("GRADE"))
+							lowest = result.getDouble("GRADE");
+						if(highest==0)
+							highest =result.getDouble("GRADE");
+						else if(highest<result.getDouble("GRADE"))
+							highest = result.getDouble("GRADE");
+					 }
+					assignmentbytype+="</tbody></table><br><h2>Highest Grade is: " + highest+"</h2><br><h2>Lowest Grade is: " + lowest+"</h2>";
+				      response.setContentType("text/html");
+					  request.setAttribute("assignmentbytype",assignmentbytype);
+				   
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+	        	 
+	        }
+	        
+	        else if(request.getParameter("action").equals("bySandT"))
+	        {
+
+	        	int count =0;
+	        	double avg = 0;
+	        	String assignmentbyst ="<table class=\"table\"><thead><tr><th>ASSIGNMENTNAME</th><th>HOMEWORKTYPE</th><th>ASSIGNMENTDATE</th><th>GRADE</th></tr></thrad><tbody>";
+	        	String getsid=request.getParameter("getsid");
+	        	String assignmtype=request.getParameter("assignmtype");
+	        	 sql ="select * from student where studentid="+Integer.parseInt(getsid)+" and HOMEWORKTYPE='"+assignmtype+"'";
+	        	 result = getResult(sql, conn);
+	        	
+	        	 try {
+
+					while(result.next())
+					 {	
+						assignmentbyst+="<tr><td>"+result.getString("ASSIGNMENTNAME")+"</td><td>"+result.getString("HOMEWORKTYPE")+"</td><td>"+result.getDate("ASSIGNMENTDATE")+"</td><td>"+result.getDouble("GRADE")+"</td></tr>"; 
+						avg+= result.getDouble("GRADE");
+						count++;
+					 }
+					avg = (avg/count);
+					assignmentbyst+="</tbody></table><br><h2>Student Average is: " + avg+"</h2>";
+				      response.setContentType("text/html");
+					  request.setAttribute("assignmentbyst",assignmentbyst);
+				   
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+	        	 
+	        }
+	        
+	      getServletContext().getRequestDispatcher("/input.jsp").forward(request, response);
+	}
+	
+	private ResultSet getResult(String sql, Connection conn )
+	{
+		ResultSet result =null;
+	    try{  
+		//creating PreparedStatement object to execute query
+	       
+	    
+		 PreparedStatement preStatement = conn.prepareStatement(sql);
+		 result = preStatement.executeQuery();
+			}
+		 catch (Exception e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		return result;
+	}
+
+}
